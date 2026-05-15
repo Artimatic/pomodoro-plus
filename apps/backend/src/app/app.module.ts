295bd3d217d9f67f2a315beb2534fa3d867fdf11
MISKATONICRESEA\dvxch <dvxchan@gmail.com>
2026-05-14 21:51:48 -0400

    updated remote paths

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,18 +1,27 @@
 // src/app/app.module.ts
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join, resolve } from 'path';
+import { join } from 'path';
 import { AppController } from './app.controller'; // if you have one
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '..', 'dist', 'apps', 'frontend', 'src', 'apps', 'shell'),
-      // For SPA routing: fallback to index.html for non-API routes
-      serveRoot: '/',
-      exclude: ['/api/(.*)'], // Don't serve static files for API routes
-    }),
+    ServeStaticModule.forRoot(
+      {
+        rootPath: join(__dirname, '..', 'frontend', 'src', 'apps', 'dashboard'),
+        serveRoot: '/dashboard',
+      },
+      {
+        rootPath: join(__dirname, '..', 'frontend', 'src', 'apps', 'settings'),
+        serveRoot: '/settings',
+      },
+      {
+        rootPath: join(__dirname, '..', 'frontend', 'src', 'apps', 'shell'),
+        serveRoot: '/', // Main Shell
+        exclude: ['/api/(.*)'],
+      },
+    ),
     // ... your other modules
   ],
   controllers: [AppController],
